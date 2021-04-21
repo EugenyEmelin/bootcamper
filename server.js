a872611cc30585b8258d4803f7e79214c2aae07b
EugenyEmelin <emelin.eug@ya.ru>
2021-04-21 08:52:00 +0300

    add logger, middleware, controllers and routes

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 //Load env vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
+
+//Connect to database
+connectDB()
 
 const app = express()
 
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 const PORT = process.env.PORT || 5000
 
-app.listen(
-    PORT, console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+  )
 )
 
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold)
+  //Close server & exit process
+  server.close(() => {
+    process.exit(1)
+  })
+})
